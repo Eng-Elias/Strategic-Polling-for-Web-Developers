afe5b5096f92207f0c56fde792ba5b5d185a55a3
Eng. Elias <eng.elias.owis@gmail.com>
2023-09-28 00:43:04 +0300

    creating server polling app

--- server_polling/server_polling/settings.py
+++ server_polling/server_polling/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # projects apps
+    "server_polling_app",
 ]
 
 MIDDLEWARE = [
---
+++ server_polling/server_polling_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ServerPollingAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'server_polling_app'
