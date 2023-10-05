1324e7474eb912dc0aaf00b246f4b977c04e2e10
Eng. Elias <eng.elias.owis@gmail.com>
2023-10-05 21:12:33 +0300

    update celery settings

--- server_polling/server_polling/settings.py
+++ server_polling/server_polling/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Celery settings
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 
 CELERY_RESULT_BACKEND = 'django-db'
 
