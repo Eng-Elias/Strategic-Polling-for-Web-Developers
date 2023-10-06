54e1c349bf976b0ea3d9fa7dff280fad4408d647
Eng. Elias <eng.elias.owis@gmail.com>
2023-10-06 20:23:29 +0300

    updating tasks

--- server_polling/server_polling_app/tasks.py
+++ server_polling/server_polling_app/tasks.py
@@ -11,6 +11,7 @@ from celery import shared_task
 def print_message(delay):
     time.sleep(delay)
     print("Message printed after {} seconds".format(delay))
+    return "Message printed after {} seconds".format(delay)
 
 
 @shared_task
@@ -31,8 +32,9 @@ def fibonacci(n):
 
 
 @shared_task(bind=True)
-def calculate_fibonacci(self, n):
-    logger.info('Task {0} started for Fibonacci number {1}'.format(self.request.id, n))
+def calculate_fibonacci(n):
+    logger.info('Task started for Fibonacci number {0}'.format(n))
     result = fibonacci(n)
-    logger.info('Task {0} finished for Fibonacci number {1}: {2}'.format(self.request.id, n, result))
+    print("Fibonacci({0}) = {1}".format(n, result))
+    logger.info('Task finished for Fibonacci number {0}: {1}'.format(n, result))
     return result
