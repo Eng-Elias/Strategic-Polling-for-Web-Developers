026d222cc6f39847fb6daa35b9ebda7ad1d8b955
Eng. Elias <eng.elias.owis@gmail.com>
2024-08-13 22:54:43 +0400

    remove fibonacci task and create n-queen task and finish all celery logic and add some styling to the frontend

--- server_polling/server_polling_app/tasks.py
+++ server_polling/server_polling_app/tasks.py
@@ -21,16 +21,35 @@ def print_message(delay, *args, **kwargs):
     return result
 
 
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+def solve_n_queens(n):
+    def is_safe(board, row, col):
+        for i in range(row):
+            if (
+                board[i] == col
+                or board[i] - i == col - row
+                or board[i] + i == col + row
+            ):
+                return False
+        return True
+
+    def solve(board, row):
+        if row == n:
+            result.append(board[:])
+            return
+        for col in range(n):
+            if is_safe(board, row, col):
+                board[row] = col
+                solve(board, row + 1)
+
+    result = []
+    solve([-1] * n, 0)
+    return result
 
 
 @shared_task(bind=True)
-def calculate_fibonacci(self, n, *args, **kwargs):
-    logger.info("Task started for Fibonacci number {0}".format(n))
-    result = fibonacci(n)
-    logger.info("Task finished for Fibonacci number {0}: {1}".format(n, result))
-    return result
+def run_n_queen_task(task, n, *args, **kwargs):
+    logger.info("Task started for {0} queens".format(n))
+    results = solve_n_queens(n)
+    # text_result = "\n".join([str(result) for result in results])
+    # logger.info("Task finished for {0} queens: {1}".format(n, text_result))
+    return results
--- server_polling/server_polling_app/views.py
+++ server_polling/server_polling_app/views.py
@@ -2,52 +2,55 @@ from django.shortcuts import render
 from django_celery_results.models import TaskResult
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .tasks import print_message, time_consuming_task, calculate_fibonacci
+from server_polling_app.tasks import (
+    print_message,
+    run_n_queen_task,
+    time_consuming_task,
+)
 from django.http import JsonResponse
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def run_simple_task(request):
-    delay = request.data.get('delay', 5)  # Default delay is 5 seconds
+    delay = int(request.data.get("delay", 5))  # Default delay is 5 seconds
     task = print_message.apply_async(args=[delay])
-    return Response({'task_id': task.id}, status=202)
+    return Response({"task_id": task.id}, status=202)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def long_polling_view(request):
     try:
         # Initiate the time-consuming Celery task
         task = time_consuming_task.apply_async()
         # Hold the response until the task is completed
         task_result = task.get()
-        print(type(task_result))
         return Response(task_result)
     except Exception as e:
-        return JsonResponse({'error': str(e)})
+        return JsonResponse({"error": str(e)})
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def run_celery_task(request):
-    n = int(request.POST.get('n', 10))  # Default Fibonacci number is 10
+    n = int(request.data.get("n", 8))  # Default number of queens is 8
 
-    # Run the Celery task
-    task = calculate_fibonacci.apply_async(args=[n])
+    # Run Celery task
+    task = run_n_queen_task.apply_async(args=[n])
     task_id = task.id
 
-    return Response({'task_id': task_id})
+    return Response({"task_id": task_id})
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def check_task_result(request, task_id):
     task_result = TaskResult.objects.filter(task_id=task_id).first()
     if task_result:
         status = task_result.status
         result = task_result.result
     else:
-        status = 'PENDING'
+        status = "PENDING"
         result = None
-    return Response({'task_id': task_id, 'status': status, 'result': result})
+    return Response({"task_id": task_id, "status": status, "result": result})
 
 
 def render_home_page(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
